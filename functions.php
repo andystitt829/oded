9ba0a5340315df36cdb1079332a89d2c8b0d3d2b
Andy Stitt <andystitt829@gmail.com>
2016-12-14 22:21:27 -0500

    enqueue bootstrap css and js from local files

--- functions.php
+++ functions.php
@@ -105,11 +105,13 @@ add_action( 'widgets_init', 'oded_widgets_init' );
 function oded_scripts() {
 	wp_enqueue_style( 'oded-style', get_stylesheet_uri() );
 
-	wp_enqueue_style( 'oded-bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' );
+	wp_enqueue_style( 'oded-bootstrap', get_template_directory_uri() . '/bootstrap/bootstrap.min.css' );
 
 	wp_enqueue_script( 'oded-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'oded-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'oded-bootstrap-js', get_template_directory_uri() . '/bootstrap/bootstrap.min.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
