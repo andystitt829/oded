590b107e525e57fc9cd8e4385f6a2b8cd3b7e41d
Andy Stitt <andystitt829@gmail.com>
2016-12-14 19:22:29 -0500

    added Bootstrap column classes to single.php and took out the site-title if statement so that it only has h1

--- header.php
+++ header.php
@@ -26,14 +26,9 @@
 	<header id="masthead" class="container site-header" role="banner">
 		<div class="row">
 			<div class="col-xs-12 col-md-8 site-branding">
-				<?php
-				if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-				endif;
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
+				<?php
 				$description = get_bloginfo( 'description', 'display' );
 				if ( $description || is_customize_preview() ) : ?>
 					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
