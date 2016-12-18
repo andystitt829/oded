51c7300f5ce7e3d1395a31f121e106bf04c95a45
Andy Stitt <andystitt829@gmail.com>
2016-12-17 20:07:27 -0500

    remove css minifier from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,10 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function(){
   return gulp.src('sass/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest(''))
 });
-gulp.task('minify-css', function() {
-  return gulp.src('style.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('production'));
-});
 
-gulp.task('default', ['sass', 'minify-css']);
+gulp.task('default', ['sass']);
