a029e5b12a7a0c18d9b3fb83e184216100f4c036
Andy Stitt <andystitt829@gmail.com>
2016-12-21 17:11:39 -0500

    entry footer styling and adding gulp tasks for build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var del = require('del');
 
 gulp.task('sass', function(){
   return gulp.src('sass/style.scss')
@@ -9,4 +10,26 @@ gulp.task('sass', function(){
 
 gulp.task('default', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('delbuild', function() {
+  return del('build/**');
+});
+
+gulp.task('move', function() {
+  console.log("Moving all files in styles folder");
+
+  gulp.src('**')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function() {
+  return del([
+    'build/.**',
+    'build/node_modules',
+    'build/sass',
+    'build/vendor',
+    'build/composer.**',
+    'build/package.json'
+  ]);
 });
