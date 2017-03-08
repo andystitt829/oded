dbf6ab52a022eed5a7cf28560332954d02358a57
Andy Stitt <andystitt829@gmail.com>
2017-03-07 21:39:21 -0500

    remove unnecessary material design and bootstrap js files

--- functions.php
+++ functions.php
@@ -119,10 +119,6 @@ function oded_scripts() {
 
 	wp_enqueue_script( 'oded-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'oded-bootstrap-js', get_template_directory_uri() . '/external/bootstrap.min.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'oded-material-js', get_template_directory_uri() . '/external/material.min.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
